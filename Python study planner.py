4a06c0ef0aa810d49ace3a0526cf88c712adf472
jerexstack <Jerexmawanda@gmail.com>
2026-06-02 18:17:10 +0300

    Update Python study planner.py
    Added JSON data persistence

--- Python study planner.py
+++ Python study planner.py
@@ -1,4 +1,10 @@
-study_planner = {}
+import json
+try:
+    with open("subjects.json", "r") as file:
+        study_planner = json.load(file)
+except FileNotFoundError:
+    study_planner = {}
+
 
 is_running = True
 while is_running:
@@ -9,17 +15,30 @@ while is_running:
     choice = input("Choose an option: ")
 
     if choice == "1":
-        subject = input("Enter the subject: ")
-        time = input("Enter the time: ")
+        subject = str(input("Enter the subject: "))
+        time = int(input("Enter the time to study for in hours: "))
         study_planner[subject] = time
         print("Subject Added!")
+
     elif choice == "2":
         print("\nStudy Plan:")
         for subject, time in study_planner.items():
-            print(f"{subject}:{time}")
+            print(f"{subject}:{time}hours")
+
+
     elif choice == "3":
+        import json
+
+        file_path = "subjects.json"
+
+        with open(file_path, "w") as file:
+            json.dump(study_planner, file, indent=4)
+            print("Data has been saved")
         print("Good Bye!")
         is_running = False
 
+
     else:
         print("Invalid Choice")
+
+
