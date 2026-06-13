675bec32060a88e8315ffb0c8df272f91a91a4e0
jerexstack <Jerexmawanda@gmail.com>
2026-06-13 18:58:50 +0300

    modified momo simulator

--- Zapier Learning/zapier_simulator2.py
+++ Zapier Learning/zapier_simulator2.py
@@ -1,11 +1,11 @@
 import time
 import momo_engine
 incoming_payloads = [
-    {"id": 101, "reference": "  s4  fEes  "},
-    {"id": 102, "reference": "momo.sch transfer"},
-    {"id": 103, "reference": "Pocket money deposit"},
+    {"id": 101, "reference": "  s4  fEes  ", "depositing": 500000},
+    {"id": 102, "reference": "momo.sch transfer", "depositing": 900000},
+    {"id": 103, "reference": "Pocket- money deposit", "depositing": 90000},
     {"id": 104, "reference": None},
-    {"id": 105, "reference": "   FEES si admission  "}
+    {"id": 105, "reference": "   FEES s1 admission--  ", "depositing": 1500000}
 ]
 print(" Starting Zapier live simulator Automatio  process...")
 start_clock = time.time()
@@ -15,8 +15,9 @@ for payload in incoming_payloads:
 
     try:
         raw_ref = payload["reference"]
+        amount = payload["depositing"]
 
-        clean_ref, routing_target = momo_engine.process_transaction(raw_ref)
+        clean_ref, routing_target = momo_engine.process_transaction(raw_ref, amount)
 
         print(f"Clean data: {clean_ref}")
         print(f"System Route: sending funds to -> {routing_target}")
