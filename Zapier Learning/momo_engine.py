e65da7835788a369a4d4b08e63b126a15f619afa
jerexstack <Jerexmawanda@gmail.com>
2026-06-13 18:12:17 +0300

    Implement  input sanitization loops and logical heierachy gates

--- Zapier Learning/momo_engine.py
+++ Zapier Learning/momo_engine.py
@@ -1,17 +1,27 @@
-def process_transaction(raw_reference):
-    """ 
-    Takes a human-typed reference string, cleans is, and returns a clean string along with its detected routing bucket
+def process_transaction(raw_reference, amount):
     """
+    Advanced Engine: Sanitizes raw input data streams and executes hierarchial routing logic with high vault tolorence.
+    """
+
+    if raw_reference is None:
+        raw_reference = ""
+
     clean_text = raw_reference.strip().replace(".", "").upper()
-    if "FEES" in clean_text:
-        category = "TUTION ACCOUNT"
-    elif "SCH" in clean_text:
-        category = "TUTION ACCOUNT"
+    noisy_characters = [".", "#", "-", "/", "_", "(", ")"]
+
+    for char in noisy_characters:
+        clean_text = clean_text.replace(char, "")
+
+    if amount >= 1000000:
+        category = "VIP HIGH-VALUE ACCOUNT"
+
+    elif "FEES" in clean_text or "SCH" in clean_text:
+        category = "TUITION ACCOUNT"
     else:
-        category = "SUSPENSE ACCOUNT"
+        category = "SUSPENCE ACCOUNT"
     return clean_text, category
-    
+
 if __name__ == "__main__":
-    print("  [TEST MODE] Running engine directly...")
-    test_text, test_cat = process_transaction("   s4  FeEs  ")
-    print(f"Test Input Cleansed to: '{test_text}' and routed to: {test_cat}")
+    print("Testing code laughing emoji's ")
+    test1, cat1 = process_transaction("   s.4 fEes--  ", 1000000)
+    print(f"Cleansed Sanitation: '{test1}' | Routing Target: '{cat1}' " )
